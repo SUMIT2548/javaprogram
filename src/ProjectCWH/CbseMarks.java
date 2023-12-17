437034ca4bd7fbbb7146b702943d4020cf4a19ee
SUMIT2548 <sumitsona35@gmail.com>
2023-12-17 22:56:56 +0530

    first

---
+++ src/ProjectCWH/CbseMarks.java
@@ -0,0 +1,17 @@
+/*Write a program to calculate the percentage of a given student in the CBSE board exam. His marks from 5 subjects must be
+taken as input from the keyboard. (Marks are out of 100).*/
+
+package ProjectCWH;
+
+import java.util.Scanner;
+
+public class CbseMarks {
+
+	public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner (System.in);
+
+
+	}
+
+}
---
+++ src/ProjectCWH/Welcome.java
@@ -0,0 +1,11 @@
+package ProjectCWH;
+
+public class Welcome {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To ! Code with Sumit")	;
+		
+	}
+
+}
