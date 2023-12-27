4482150321ad73a926fa175d809c13d681a2298d
SUMIT2548 <sumitsona35@gmail.com>
2023-12-27 23:46:53 +0530

    java

---
+++ src/ProjectCWH/PracticeSet4.java
@@ -0,0 +1,38 @@
+package ProjectCWH;
+
+public class PracticeSet4 {
+
+	public static void main(String[] args)
+	{
+	/*	Question 1: Write a program to print the following pattern :
+			****
+			***
+			**
+			*
+      
+		   int n = 4;
+           for (int i=n; i>0; i--)
+           {
+              for(int j=0;j<i;j++)
+              {
+                 System.out.print("*");
+              }
+            System.out.print("\n");
+           }
+      */     
+  
+	//Question 2: Write a program to sum first n even numbers using a while loop.  
+           
+           int n =10;
+           int a = 1;
+           while(a<=n)
+           {
+        	   System.out.println(2*a);
+        	   a++;
+        	   
+           }
+           
+           
+           
+	} 
+}
